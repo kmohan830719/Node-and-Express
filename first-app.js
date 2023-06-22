454e95af2dcb75c1276fbfa77acda5f37c71a17f
K Mohan <km830719@gmail.com>
2023-06-22 19:02:28 +0530

    added an student object

--- first-app.js
+++ first-app.js
@@ -7,3 +7,14 @@ let num2=3;
 const product=(n1,n2)=>n1*n2;
 
 console.log(product(num1,num2));
+
+//create a student object
+const studenet={
+    name:"Mohan",
+    age:25,
+    getDetails: function(){
+      console.log(`My name is ${this.name} and age is ${this.age}`)
+    }
+};
+
+studenet.getDetails();
