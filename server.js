c82ef989226fae2c3d25727eb950acfdbff02557
K Mohan <km830719@gmail.com>
2023-06-28 01:37:22 +0530

    returning custom responses based on url

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
-     console.log("Mohan")
+     const url=req.url;
+     if(url === '/home'){
+        res.write('<html>');
+        res.write('<head><title>My first page</title><head>');
+        res.write('<body><h1>Welcome to Home</h1></body>');
+        res.write('</html>');
+        return res.end();
+     }
+     if(url === '/about'){
+        res.write('<html>');
+        res.write('<head><title>My first page</title><head>');
+        res.write('<body><h1>Welcome to about us page</h1></body>');
+        res.write('</html>');
+        return res.end();
+     }
+     if(url === '/node'){
+        res.write('<html>');
+        res.write('<head><title>My first page</title><head>');
+        res.write('<body><h1>Welcome to my Node.js project </h1></body>');
+        res.write('</html>');
+        return res.end();
+     }
 })
 server.listen(4000);
