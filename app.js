ab603c8ac7d38a129bf7c8058dcfd5cad5005613
K Mohan <km830719@gmail.com>
2023-06-28 07:12:53 +0530

    added routes

--- app.js
+++ app.js
@@ -1,34 +1,6 @@
 const http=require('http');
-const fs=require('fs');
+const routes=require('./routes.js');
 
-const server=http.createServer((req,res)=>{
-    const url=req.url;
-    const method=req.method;
-    if(url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title><head>');
-        const readMsg = fs.readFileSync('message.text', 'utf-8');
-        res.write('<h1>' + readMsg + '</h1>');
-        res.write('<body><form action="/message" method="POST"><input typr="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if(url === "/message" && method==="POST"){
-        const body=[];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end',()=>{
-        const parseBody=Buffer.concat(body).toString();
-        const message=parseBody.split('=')[1];
-        fs.writeFileSync("message.text" , message);
-        });
-        res.statusCode=302;
-        res.setHeader('Location','/');
-        return res.end();
-    }
-       
-})
-
+const server=http.createServer(routes.handler);
+   
 server.listen(4000);
