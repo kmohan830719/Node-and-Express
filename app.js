10305c114e36fca910d61d1d1f54541575973f21
K Mohan <km830719@gmail.com>
2023-07-12 10:24:10 +0530

    creaed /add-product route

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const http=require('http');
 
 const express= require('express');
+const bodyPrser=require('body-parser');
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware')
-    next(); //allows the request to continue to the next middleware in line
+app.use(bodyPrser.urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">add product<button/><form/>');
 })
 
-app.use((req,res,next)=>{
-    console.log('In another middleware')
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
     res.send('<h1>hello from express!</h1>')
 })
 
-const server=http.createServer(app);
-
-server.listen(4000);
+app.listen(4000);
